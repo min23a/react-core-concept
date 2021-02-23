e49067ae2dcc3b2702360577928fa81e36da55fe
min23a <minhazuloabedin0611@gmail.com>
2021-02-23 22:14:59 +0600

    banner updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="">
       <Menu menu1="Home" menu2="Shop" menu3="About"></Menu>
-      <Slider img1="https://lh3.googleusercontent.com/proxy/2AALcZfrTnS_qd78FCRDGB4Be8m_Sh_s4xQIByXJwyFjl8JTuH6nP7-z7uw03sXOF3YePOTrkDf0HU2hAcYVF07rvDB_rJ3YufSSHb3jdl8" img2="https://thumbs.dreamstime.com/b/backpack-different-colorful-stationery-table-banner-design-180023783.jpg" img3="https://i.pinimg.com/originals/44/a8/96/44a896b49c923eacb70404f85cada62a.jpg" img4="https://www.pngkey.com/png/detail/249-2499302_online-shopping-clothing-shoes-mens-fashion-womens-women.png"></Slider>
+      <Banner img="https://lh3.googleusercontent.com/proxy/2AALcZfrTnS_qd78FCRDGB4Be8m_Sh_s4xQIByXJwyFjl8JTuH6nP7-z7uw03sXOF3YePOTrkDf0HU2hAcYVF07rvDB_rJ3YufSSHb3jdl8" ></Banner>
     </div>
   );
 }
@@ -20,17 +20,18 @@ function Menu(props) {
     </div>
   );
 }
-function Slider(props) {
-  const images = [props.img1, props.img2, props.img3, props.img4];
-  let i = 0;
-  const URls = `url('${images[i]}')`;
-  const style = { backgroundImage: URls }
+function Banner(props) {
+    const images = props.img;
+    const URls = `url('${images}')`;
+    const style = { backgroundImage: URls }
   return (
-    <section className="slider">
-      <div className="slider_controls">
-      </div>
-      <div className="slider_content" style={style}>
-
+    <section className="banner">
+      <div className="banner_content" style={style}>
+        <div className="banner_overlay">
+          <p className="banner_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quibusdam animi provident, nostrum inventore vero corrupti. Est rem officia voluptas. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolorem. Ea, alias placeat sint minus aut natus, modi corrupti quos soluta tenetur perferendis vero maxime officia beatae odit, et cum?</p>
+        <button className="banner_btn">SHOP NOW</button>
+        </div>
+        
       </div>
     </section>
   );
