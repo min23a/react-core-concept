cd2f72b81dafd73e83bc601b08de5004cc12ef71
min23a <minhazuloabedin0611@gmail.com>
2021-02-24 17:23:35 +0600

    featured product added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="">
       <Menu menu1="Home" menu2="Shop" menu3="About"></Menu>
-      <Banner img="https://lh3.googleusercontent.com/proxy/2AALcZfrTnS_qd78FCRDGB4Be8m_Sh_s4xQIByXJwyFjl8JTuH6nP7-z7uw03sXOF3YePOTrkDf0HU2hAcYVF07rvDB_rJ3YufSSHb3jdl8" ></Banner>
+      <Banner img="media/mens-fashion-womens-women.png" ></Banner>
+      <FeaturedProduct Link='https://fakestoreapi.com/products?limit=6'></FeaturedProduct>
     </div>
   );
 }
 function Menu(props) {
   return (
-    <div className="menu">
+    <nav className="menu">
       <a className="brand_link" href="#"><img className="brand_logo" src="media/logo.png" alt="" /></a>
       <ul className="menu_list">
         <li ><a className="menu_item" href="#">{props.menu1}</a></li>
         <li><a className="menu_item" href="#">{props.menu2}</a></li>
         <li><a className="menu_item" href="#">{props.menu3}</a></li>
       </ul>
-    </div>
+    </nav>
   );
 }
 function Banner(props) {
-    const images = props.img;
-    const URls = `url('${images}')`;
-    const style = { backgroundImage: URls }
+  const images = props.img;
+  const URls = `url('${images}')`;
+  const style = { backgroundImage: URls }
   return (
     <section className="banner">
       <div className="banner_content" style={style}>
         <div className="banner_overlay">
           <p className="banner_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quibusdam animi provident, nostrum inventore vero corrupti. Est rem officia voluptas. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolorem. Ea, alias placeat sint minus aut natus, modi corrupti quos soluta tenetur perferendis vero maxime officia beatae odit, et cum?</p>
-        <button className="banner_btn">SHOP NOW</button>
+          <button className="banner_btn">SHOP NOW</button>
         </div>
-        
       </div>
     </section>
+  );
+}
+function FeaturedProduct(props) {
+  const address = props.Link;
+  fetch(address)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(element => {
+        console.log(element)
+        const divs = document.createElement("div");
+        divs.className = "products";
+        divs.style.backgroundImage = `url('${element.image}')`
+        document.querySelector('.featured_product').appendChild(divs);
+      });
+    })
+  return (
+    <section>
+      <h2 className="title">Featured Product</h2>
+      <div className="featured_product">
+
+      </div>
+    </section>
+
   );
 }
 
